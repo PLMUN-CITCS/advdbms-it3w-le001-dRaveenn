932bd2ea7dda0cb5b557f50fe8e50c0dae62915a
Raven Darius Flores <floresravendarius_bsit@plmun.edu.ph>
2025-09-04 10:37:19 +0800

    Submitting ADVDBMS Week 04 - Session 01 - Exercise 01

--- university_db/drop_db.sql
+++
@@ -1,6 +0,0 @@
--- Step 3: (Optional) Drop the database if needed. 
--- Use with extreme caution!
-USE `UniversityDB`;
-
--- The IF EXISTS clause prevents an error if the database doesn't exist.
-DROP DATABASE IF EXISTS `UniversityDB`;
